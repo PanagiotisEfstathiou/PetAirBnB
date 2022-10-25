c56e8322b17e3f47573b5ba3365f521efa27caec
panagiotisefstathiou <pefstathiou@outlook.com>
2022-10-25 19:26:45 +0300

    Initial commit

---
+++ src/main/java/com/edu/petairbnb/PetAirBnBApplication.java
@@ -0,0 +1,13 @@
+package com.edu.petairbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAirBnBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetAirBnBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/petairbnb/PetAirBnBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.petairbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAirBnBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
