dfad4a2fe22a8e2c948075352fdb9c3f87ec7f5c
panagiotisefstathiou <pefstathiou@outlook.com>
2022-10-25 19:50:48 +0300

    Adds cmment

--- src/main/java/com/edu/petairbnb/PetAirBnBApplication.java
+++ src/main/java/com/edu/petairbnb/PetAirBnBApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PetAirBnBApplication {
-
+//ashgdashj
 	public static void main(String[] args) {
+
 		SpringApplication.run(PetAirBnBApplication.class, args);
 	}
 
