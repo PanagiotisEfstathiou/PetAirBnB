6aa5ad317f1bb2a7f9c4decdfc908dc76f6d656a
Phevos Vlachos <74672099+PhoebusVlachos@users.noreply.github.com>
2022-10-25 19:43:34 +0300

    Comment

--- src/main/java/com/edu/petairbnb/PetAirBnBApplication.java
+++ src/main/java/com/edu/petairbnb/PetAirBnBApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PetAirBnBApplication {
+	//Phevos says hello!
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetAirBnBApplication.class, args);
