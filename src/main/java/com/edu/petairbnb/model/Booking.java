8c51c8404f821640f3e1f7354ed77f36c8085a61
fotis <fotiskhougaz@gmail.com>
2022-10-26 20:49:01 +0300

    Completed Booking Class Spring Notations

--- src/main/java/com/edu/petairbnb/model/Booking.java
+++ src/main/java/com/edu/petairbnb/model/Booking.java
@@ -1,20 +1,40 @@
 package com.edu.petairbnb.model;
 
+import com.sun.istack.NotNull;
+import lombok.*;
+
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import java.util.Date;
 import java.util.List;
 
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@ToString(callSuper = true)
 public class Booking extends BaseModel{
 
     @Column(length = 20, nullable = false)
+    @NotNull
     private Date date;
 
+    @OneToMany
     @Column
+    @NotNull
     private List<Animal> pets;
 
+    @OneToOne
     @Column(nullable = false)
+    @NotNull
     private Account customer;
 
+    @OneToOne
     @Column(nullable = false)
+    @NotNull
     private Carer carer;
 }
