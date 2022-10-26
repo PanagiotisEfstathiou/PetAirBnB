40d70f137e67b830598ee396aa1bd80ea69dd9b7
panagiotisefstathiou <pefstathiou@outlook.com>
2022-10-26 21:21:26 +0300

    Adds the accountRepository

---
+++ src/main/java/com/edu/petairbnb/repository/AccountRepository.java
@@ -0,0 +1,7 @@
+package com.edu.petairbnb.repository;
+
+import com.edu.petairbnb.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account,Long> {
+}
