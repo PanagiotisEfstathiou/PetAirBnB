4a3a9210b379900456a8bf28f3c9fd98d3b52b62
panagiotisefstathiou <pefstathiou@outlook.com>
2022-10-27 12:47:14 +0300

    Adds @Component and @AllArgsConstructor annotations to the AccountServiceImpl

--- src/main/java/com/edu/petairbnb/service/AccountServiceImpl.java
+++ src/main/java/com/edu/petairbnb/service/AccountServiceImpl.java
@@ -2,15 +2,17 @@ package com.edu.petairbnb.service;
 
 import com.edu.petairbnb.model.Account;
 import com.edu.petairbnb.repository.AccountRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
+@Component
+@AllArgsConstructor
 public class AccountServiceImpl extends BaseServiceImpl<Account> implements AccountService {
 
     private AccountRepository accountRepository;
     @Override
-    public JpaRepository<Account, Long> getRepository() {
-        return accountRepository;
-    }
+    public JpaRepository<Account, Long> getRepository() {return accountRepository;}
 }
