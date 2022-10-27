236f0a7095d81cd7d99adce30d2817c19a1796c3
panagiotisefstathiou <pefstathiou@outlook.com>
2022-10-27 12:48:21 +0300

    Implements Booking Service and Booking Service Impl

---
+++ src/main/java/com/edu/petairbnb/service/BookingService.java
@@ -0,0 +1,8 @@
+package com.edu.petairbnb.service;
+
+import com.edu.petairbnb.model.Booking;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface BookingService extends BaseService<Booking> {
+}
---
+++ src/main/java/com/edu/petairbnb/service/BookingServiceImpl.java
@@ -0,0 +1,19 @@
+package com.edu.petairbnb.service;
+
+import com.edu.petairbnb.model.Booking;
+import com.edu.petairbnb.repository.BookingRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class BookingServiceImpl extends BaseServiceImpl<Booking> implements BookingService {
+
+    private BookingRepository bookingRepository;
+
+    @Override
+    public JpaRepository<Booking, Long> getRepository() {
+        return bookingRepository;
+    }
+}
