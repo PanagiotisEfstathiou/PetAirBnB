60590a905993d177c3875f096976fc93886636ef
panagiotisefstathiou <pefstathiou@outlook.com>
2022-10-27 12:49:03 +0300

    Implements Animal Service

---
+++ src/main/java/com/edu/petairbnb/service/AnimalService.java
@@ -0,0 +1,8 @@
+package com.edu.petairbnb.service;
+
+import com.edu.petairbnb.model.Animal;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface AnimalService extends BaseService<Animal> {
+}
