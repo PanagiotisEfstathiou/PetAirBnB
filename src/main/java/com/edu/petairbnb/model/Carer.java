c46e29d10495e936e120b27cf8318137b491c09c
panagiotisefstathiou <pefstathiou@outlook.com>
2022-10-27 12:48:46 +0300

    Comments out all carer classes

--- src/main/java/com/edu/petairbnb/model/Carer.java
+++ src/main/java/com/edu/petairbnb/model/Carer.java
@@ -1,23 +1,23 @@
-package com.edu.petairbnb.model;
-
-import lombok.*;
-
-import javax.persistence.*;
-import java.util.List;
-
-@Setter
-@Getter
-@ToString(callSuper = true)
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-public class Carer extends Account {
-
-    @Column
-    @ElementCollection
-    private List<String> rules;
-
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<Animal> petHistory;
-}
+//package com.edu.petairbnb.model;
+//
+//import lombok.*;
+//
+//import javax.persistence.*;
+//import java.util.List;
+//
+//@Setter
+//@Getter
+////@ToString(callSuper = true)
+////@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Entity
+//public class Carer extends Account {
+//
+//    @Column
+//    @ElementCollection
+//    private List<String> rules;
+//
+//    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+//    private List<Animal> petHistory;
+//}
---
+++ src/main/java/com/edu/petairbnb/service/CarerService.java
@@ -0,0 +1,8 @@
+//package com.edu.petairbnb.service;
+//
+//import com.edu.petairbnb.model.Carer;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public interface CarerService extends BaseService<Carer> {
+//}
