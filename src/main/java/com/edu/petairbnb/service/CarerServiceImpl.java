cd5a91431605bc9213666ee80a44842f373fdb56
Phevos Vlachos <74672099+PhoebusVlachos@users.noreply.github.com>
2022-10-30 13:21:14 +0200

    Implemented Carer Service and Implementation

--- src/main/java/com/edu/petairbnb/service/CarerServiceImpl.java
+++ src/main/java/com/edu/petairbnb/service/CarerServiceImpl.java
@@ -1,18 +1,19 @@
-//package com.edu.petairbnb.service;
-//
-//import com.edu.petairbnb.model.Carer;
-//import com.edu.petairbnb.repository.CarerRepository;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@AllArgsConstructor
-//public class CarerServiceImpl extends BaseServiceImpl<Carer> implements CarerService{
-//
-//    private CarerRepository carerRepository;
-//
-//    @Override
-//    public JpaRepository<Carer, Long> getRepository() {
-//        return carerRepository;
-//    }
-//}
+package com.edu.petairbnb.service;
+
+import com.edu.petairbnb.model.Carer;
+import com.edu.petairbnb.repository.CarerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CarerServiceImpl extends BaseServiceImpl<Carer> implements CarerService{
+
+    private CarerRepository carerRepository;
+
+    @Override
+    public JpaRepository<Carer, Long> getRepository() {
+        return carerRepository;
+    }
+}
