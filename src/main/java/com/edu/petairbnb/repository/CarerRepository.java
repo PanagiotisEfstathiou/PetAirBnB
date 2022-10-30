6d8fee6ada0389ce88df580436036440bd7d1096
Phevos Vlachos <74672099+PhoebusVlachos@users.noreply.github.com>
2022-10-30 13:21:42 +0200

    Implemented Carer Repository

--- src/main/java/com/edu/petairbnb/repository/CarerRepository.java
+++ src/main/java/com/edu/petairbnb/repository/CarerRepository.java
@@ -1,8 +1,9 @@
-//package com.edu.petairbnb.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface CarerRepository extends JpaRepository {
-//}
+package com.edu.petairbnb.repository;
+
+import com.edu.petairbnb.model.Carer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarerRepository extends JpaRepository<Carer, Long>{
+}
