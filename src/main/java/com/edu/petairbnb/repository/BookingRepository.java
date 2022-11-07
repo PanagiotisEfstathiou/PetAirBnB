782595962bbb4e33d1a5ea377235add836f90106
panagiotisefstathiou <pefstathiou@outlook.com>
2022-11-07 13:51:45 +0200

    Adds a query to get the booking history of a user in the booking repository

--- src/main/java/com/edu/petairbnb/repository/BookingRepository.java
+++ src/main/java/com/edu/petairbnb/repository/BookingRepository.java
@@ -2,8 +2,17 @@ package com.edu.petairbnb.repository;
 
 import com.edu.petairbnb.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.QueryHint;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking,Long> {
+
+    @Query("select o from Booking o join fetch o.customer where o.id = :id")
+    @QueryHints(@QueryHint(name = org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH, value = "false"))
+    Booking bookingHistory(Long id);
+
 }
