bcc7b698aa6654e0e1f6c577f44b83c3774eee47
panagiotisefstathiou <pefstathiou@outlook.com>
2022-11-07 13:52:54 +0200

    Deletes everything associated with the carer class

--- src/main/java/com/edu/petairbnb/repository/CarerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.edu.petairbnb.repository;
-
-import com.edu.petairbnb.model.Carer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarerRepository extends JpaRepository<Carer, Long>{
-}
--- src/main/java/com/edu/petairbnb/service/CarerService.java
+++
@@ -1,8 +0,0 @@
-package com.edu.petairbnb.service;
-
-import com.edu.petairbnb.model.Carer;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface CarerService extends BaseService<Carer> {
-}
