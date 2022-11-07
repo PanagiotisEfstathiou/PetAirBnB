0ceba6392759d0e4d36ee72bedbeb4f413df22dd
panagiotisefstathiou <pefstathiou@outlook.com>
2022-11-07 13:49:22 +0200

    Adds a comment in the animal service class

--- src/main/java/com/edu/petairbnb/service/AnimalService.java
+++ src/main/java/com/edu/petairbnb/service/AnimalService.java
@@ -5,4 +5,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface AnimalService extends BaseService<Animal> {
+
+    //getAnimalStatus(ID)
+        //Must add to the animal object an enum that describes the status of an animal. e.g. if it is not in a carer house status = home.
+
+
 }
