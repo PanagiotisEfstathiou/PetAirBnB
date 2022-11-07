3a2b6994416d19b0a28d01899128b532c08a59a0
panagiotisefstathiou <pefstathiou@outlook.com>
2022-11-07 13:53:37 +0200

    Edits the SampleContentCreator class to build some more objects

--- src/main/java/com/edu/petairbnb/bootstrap/SampleContentCreator.java
+++ src/main/java/com/edu/petairbnb/bootstrap/SampleContentCreator.java
@@ -7,7 +7,6 @@ import com.edu.petairbnb.service.AnimalService;
 //import com.edu.petairbnb.service.BookingService;
 //import com.edu.petairbnb.service.CarerService;
 import com.edu.petairbnb.service.BookingService;
-import com.edu.petairbnb.service.CarerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
@@ -24,30 +23,35 @@ public class SampleContentCreator extends BaseComponent implements CommandLineRu
     private final AccountService accountService;
     private final AnimalService animalService;
     private final BookingService bookingService;
-    private final CarerService carerService;
+
 
     @Override
     public void run(String... args) throws Exception{
 
         logger.warn("adadasdasdasdas");
 
-        List<String> reviews = List.of("a");
+        List<String> reviews = List.of("a","b","c","d","f");
+        List<String> reviews2 = List.of("a");
 
-        List<Account> account = List.of(Account.builder().firstName("first").lastName("last").email("email").password("password").reviews(reviews).build());
 
-        List<Animal> animals = List.of(Animal.builder().animalType(AnimalType.valueOf("DOG")).age(1).comments("aaa").owner(account.get(0)).build());
+
+        List<Account> account = List.of(Account.builder().firstName("first").lastName("last").email("email").password("password").reviews(reviews).build(),
+                Account.builder().firstName("second").lastName("secondLastName").email("email2").password("password").reviews(reviews2).build(),
+                Account.builder().firstName("third").lastName("thirdLastName").email("email3").password("password").expertise(Expertise.DOG).build());
+
+        List<Animal> animals = List.of(Animal.builder().name("max").animalType(AnimalType.valueOf("DOG")).age(1).comments("aaa").owner(account.get(0)).build());
         List<String> rules = List.of("a");
-        List<Carer> carers = List.of(Carer.builder().rules(rules).petHistory(animals).firstName("FirstName").lastName("LastName").email("Email").password("Password").reviews(reviews).build());
 
-        account.forEach(account1 -> account1.setPetsOwned(animals));
+        //account.forEach(account1 -> account1.setPetsOwned(animals));
+
+        account.get(0).setPetsOwned(animals);
 
 
         accountService.createAll(account);
 
-        Booking booking = Booking.builder().date(new Date()).customer(account.get(0)).pets(animals).build();
+        Booking booking = Booking.builder().date(new Date()).customer(account.get(0)).carer(account.get(1)).pets(animals).build();
         bookingService.createAll(booking);
 
-        carerService.createAll(carers);
 
     }
 }
