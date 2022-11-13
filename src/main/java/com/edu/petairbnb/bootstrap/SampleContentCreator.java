8724db35e9f4d01ca654bf7069bc7f0bb2290794
panagiotisefstathiou <pefstathiou@outlook.com>
2022-11-13 11:46:11 +0200

    Changes Sample content creator

--- src/main/java/com/edu/petairbnb/bootstrap/SampleContentCreator.java
+++ src/main/java/com/edu/petairbnb/bootstrap/SampleContentCreator.java
@@ -2,11 +2,9 @@ package com.edu.petairbnb.bootstrap;
 
 import com.edu.petairbnb.base.BaseComponent;
 import com.edu.petairbnb.model.*;
-import com.edu.petairbnb.service.AccountService;
-import com.edu.petairbnb.service.AnimalService;
+import com.edu.petairbnb.service.*;
 //import com.edu.petairbnb.service.BookingService;
 //import com.edu.petairbnb.service.CarerService;
-import com.edu.petairbnb.service.BookingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
@@ -14,15 +12,16 @@ import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.List;
-import java.util.logging.Logger;
 
 @Component
 @Profile("generateSampleData")
 @RequiredArgsConstructor
 public class SampleContentCreator extends BaseComponent implements CommandLineRunner {
-    private final AccountService accountService;
+    private final OwnerService ownerService;
     private final AnimalService animalService;
     private final BookingService bookingService;
+    private final CarerService carerService;
+    private final ReviewService reviewService;
 
 
     @Override
@@ -30,26 +29,35 @@ public class SampleContentCreator extends BaseComponent implements CommandLineRu
 
         logger.warn("adadasdasdasdas");
 
-        List<String> reviews = List.of("a","b","c","d","f");
-        List<String> reviews2 = List.of("a");
 
 
 
-        List<Account> account = List.of(Account.builder().firstName("first").lastName("last").email("email").password("password").reviews(reviews).build(),
-                Account.builder().firstName("second").lastName("secondLastName").email("email2").password("password").reviews(reviews2).build(),
-                Account.builder().firstName("third").lastName("thirdLastName").email("email3").password("password").expertise(Expertise.DOG).build());
 
-        List<Animal> animals = List.of(Animal.builder().name("max").animalType(AnimalType.valueOf("DOG")).age(1).comments("aaa").owner(account.get(0)).build());
+
+        List<Owner> owner = List.of(Owner.builder().firstName("first").lastName("last").email("email").password("password").build(),
+                Owner.builder().firstName("second").lastName("secondLastName").email("email2").password("password").build(),
+                Owner.builder().firstName("third").lastName("thirdLastName").email("email3").password("password").build());
+
+        List<Animal> animals = List.of(Animal.builder().name("max").animalType(AnimalType.valueOf("DOG")).age(1).comments("aaa").owner(owner.get(0)).build());
         List<String> rules = List.of("a");
 
-        //account.forEach(account1 -> account1.setPetsOwned(animals));
+        //owner.forEach(account1 -> account1.setPetsOwned(animals));
 
-        account.get(0).setPetsOwned(animals);
+        owner.get(0).setPetsOwned(animals);
+        //owner.get(0).setReviewsWritten(reviews);
 
 
-        accountService.createAll(account);
+        Carer carer = Carer.builder().firstName("first").lastName("last").email("email").password("password").build();
 
-        Booking booking = Booking.builder().date(new Date()).customer(account.get(0)).carer(account.get(1)).pets(animals).build();
+        List<Review> reviews = List.of(Review.builder().reviewType(ReviewType.GOOD).content("aaa").date(new Date()).build());
+        reviews.get(0).setAuthor(owner.get(0));
+        reviews.get(0).setReceiver(carer);
+        //carer.setReview(reviews);
+        carerService.create(carer);
+        ownerService.createAll(owner);
+        animalService.createAll(animals);
+        reviewService.createAll(reviews);
+        Booking booking = Booking.builder().date(new Date()).customer(owner.get(0)).carer(carer).pet(animals.get(0)).build();
         bookingService.createAll(booking);
 
 
